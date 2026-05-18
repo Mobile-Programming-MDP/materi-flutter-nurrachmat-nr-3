efca1842caebc9a9a13b8a76c220b3dd6891a7a1
Nur Rachmat <rachmat.nur91@gmail.com>
2026-05-18 16:04:34 +0700

    Set Android SDK/NDK and enable multidex
    Replace Flutter-provided SDK/NDK variables with explicit values (compileSdk=35, ndkVersion="27.0.12077973"). Set targetSdk=35, versionCode=1 and versionName="1.0.0", and enable multiDex in defaultConfig. Add androidx.multidex:multidex:2.0.1 dependency and remove leftover commented desugaring/TODO lines to stabilize Android build configuration.

--- cepu_app/android/app/build.gradle.kts
+++ cepu_app/android/app/build.gradle.kts
@@ -10,11 +10,10 @@ plugins {
 
 android {
     namespace = "com.example.cepu_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        //isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -24,14 +23,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.cepu_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -43,9 +40,9 @@ android {
     }
 }
 
-// dependencies{
-//     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-// }
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
+}
 
 flutter {
     source = "../.."
